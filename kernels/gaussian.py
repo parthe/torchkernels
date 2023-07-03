d8e00a771ce01c266461688fa13b01c27291258f
Parthe Pandit <parthe@users.noreply.github.com>
2023-07-03 13:16:21 -0700

    Update gaussian.py

--- kernels/gaussian.py
+++ kernels/gaussian.py
@@ -1,6 +1,6 @@
 import primitives
 
-def gaussian(samples, centers, bandwidth):
+def gaussian(samples, centers, bandwidth=1., M=None):
     '''Gaussian kernel.
 
     Args:
@@ -12,7 +12,7 @@ def gaussian(samples, centers, bandwidth):
         kernel matrix of shape (n_sample, n_center).
     '''
     assert bandwidth > 0
-    kernel_mat = primitives.euclidean(samples, centers)
+    kernel_mat = primitives.euclidean(samples, centers, squared=True, M=M)
     kernel_mat.clamp_(min=0)
     gamma = 1. / (2 * bandwidth ** 2)
     kernel_mat.mul_(-gamma)
