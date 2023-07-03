fe4a19972f88fd623754054d122f84f1187db04e
Parthe Pandit <parthe@users.noreply.github.com>
2023-07-03 12:33:10 -0700

    Create laplacian.py

---
+++ kernels/laplacian.py
@@ -0,0 +1,18 @@
+def laplacian(samples, centers, bandwidth):
+    '''Laplacian kernel.
+
+    Args:
+        samples: of shape (n_sample, n_feature).
+        centers: of shape (n_center, n_feature).
+        bandwidth: kernel bandwidth.
+
+    Returns:
+        kernel matrix of shape (n_sample, n_center).
+    '''
+    assert bandwidth > 0
+    kernel_mat = euclidean(samples, centers, squared=False)
+    kernel_mat.clamp_(min=0)
+    gamma = 1. / bandwidth
+    kernel_mat.mul_(-gamma)
+    kernel_mat.exp_()
+    return kernel_mat
