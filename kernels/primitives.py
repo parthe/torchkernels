423e62d7b1f4c3658b19d1bbfdb8dc9feb02ed29
Parthe Pandit <parthe@users.noreply.github.com>
2023-07-03 13:15:41 -0700

    Update primitives.py

--- kernels/primitives.py
+++ kernels/primitives.py
@@ -2,30 +2,58 @@ import torch
 
 eps = 1e-12
 
-def euclidean(samples, centers, squared=True):
-    '''Calculate the pointwise distance.
+def norm(X, squared=False, M=None):
+    '''Calculate the norm.
+    If `M` is not None, then Mahalanobis norm is calculated.
 
     Args:
-        samples: of shape (n_sample, n_feature).
-        centers: of shape (n_center, n_feature).
+        X: of shape (n, d).
+        M: of shape (d, d). positive semi-definite matrix for Mahalanobis norm.
         squared: boolean.
 
     Returns:
-        pointwise distances (n_sample, n_center).
+        pointwise norm (n,).
     '''
-    samples_norm = torch.sum(samples**2, dim=1, keepdim=True)
-    if samples is centers:
-        centers_norm = samples_norm
+    if M is None:
+        X2 = torch.sum(X**2, dim=1) if squared:    
+        return X2 if squared else X2.sqrt()
     else:
-        centers_norm = torch.sum(centers**2, dim=1, keepdim=True)
-    centers_norm = torch.reshape(centers_norm, (1, -1))
+        XMX = ((X @ M) * X).sum(dim=-1)
+        return XMX if squared else XMX.sqrt()
 
-    distances = samples.mm(torch.t(centers))
+def inner_product(samples, centers, M=None):
+    '''Calculate the pairwise inner-product.
+     If `M` is not None, then Mahalanobis inner-product is calculated
+
+    Args:
+        samples: of shape (n, d).
+        centers: of shape (p, d).
+        M: of shape (d, d). positive semi-definite matrix for Mahalnobis inner-product.
+
+    Returns:
+        pointwise distances (n, p).
+    '''
+    return samples @ centers.T if M is None else samples @ M @ centers.T
+
+def euclidean(samples, centers, squared=False, M=None):
+    '''Calculate the pairwise euclidean distance.
+     If `M` is not None, then Mahalanobis distance is calculated.
+
+    Args:
+        samples: of shape (n, d).
+        centers: of shape (p, d).
+        squared: boolean.
+        M: of shape (d, d). positive semi-definite matrix for Mahalanobis norm.
+
+    Returns:
+        pointwise distances (n, p).
+    '''
+    samples_norm2 = norm(samples, squared=True, M=M)
+    centers_norm2 = samples_norm2 if samples is centers else norm(centers, squared=True, M=M)
+    
+    distances = inner_product(samples, centers, M=M)
     distances.mul_(-2)
-    distances.add_(samples_norm)
-    distances.add_(centers_norm)
-    if not squared:
-        distances.clamp_(min=0)        
-        distances.sqrt_()
+    distances.add_(samples_norm2.view(-1, 1))
+    distances.add_(centers_norm2)
 
-    return distances
+    return distances if squared else distances.clamp_(min=0).sqrt_()
