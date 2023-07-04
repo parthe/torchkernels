ff0acf441f04d87cb39187601c9623b322c42078
Parthe Pandit <parthe@users.noreply.github.com>
2023-07-03 17:02:25 -0700

    Create general.py

---
+++ kernels/general.py
@@ -0,0 +1,10 @@
+from .primitives import norm, inner_product
+
+def normalized_inner_product(func, samples, centers, M=None):
+    """
+        K(x,z) = norm(x) * norm(z) * func(<x_, z_>) where x_ = x/norm(x) and z_ = z/norm(z)
+    """
+    x_norm = norm(x, M=M)
+    z_norm = norm(z, M=M)
+    xz = inner_product(x/norm(x).view(-1,1), z/norm(z).view(-1,1), M=M)
+    return x_norm.view(-1,1) * func(xz) * z_norm
