6fcb149286864820f26691c34803c8e2d05ba6c3
Parthe Pandit <parthe@users.noreply.github.com>
2023-07-05 11:11:17 -0700

    Update fmm.py

--- kernel_utils/primitives/fmm.py
+++ kernel_utils/primitives/fmm.py
@@ -3,6 +3,7 @@ import torch, math
 def kmat_vec(K, X, Z, v, out=None, row_chunk_size=None, col_chunk_size=None):
     """
         calculate kernel matrix vector product K(X, Z) @ v without storing kernel matrix
+        If argument `out` is provided, the result is added to `out`
     """
     n, p = len(X), len(Z)
     b_n = n if row_chunk_size is None else row_chunk_size
@@ -13,7 +14,7 @@ def kmat_vec(K, X, Z, v, out=None, row_chunk_size=None, col_chunk_size=None):
 
     for i in range(math.ceil(n/b_n)):
         for j in range(math.ceil(n/b_n)):
-            out[i*b_n:(i+1)*b_n] = K(X[i*b_n:(i+1)*b_n], Z[j*b_p:(j+1)*b_p]) @ v[j*b_p:(j+1)*b_p]
+             out[i*b_n:(i+1)*b_n] += K(X[i*b_n:(i+1)*b_n], Z[j*b_p:(j+1)*b_p]) @ v[j*b_p:(j+1)*b_p]
 
     if out is None:
         return out
