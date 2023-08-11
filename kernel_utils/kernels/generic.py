a5138c77cc1e8b20aa39f669be247f1c099ab39a
Parthe Pandit <parthe@users.noreply.github.com>
2023-08-11 08:48:11 -0700

    Update generic.py

--- kernel_utils/kernels/generic.py
+++ kernel_utils/kernels/generic.py
@@ -1,9 +1,10 @@
 from ..linalg import norm, inner_product
 
-def normalized_inner_product(func, x, z, M=None):
+def normalized_inner_product(func, x, z=None, M=None):
     """
         K(x,z) = norm(x) * norm(z) * func(<x_, z_>) where x_ = x/norm(x) and z_ = z/norm(z)
     """
+    if z is None: z = x
     x_norm = norm(x, M=M)
     z_norm = norm(z, M=M)
     xz = inner_product(x/x_norm.view(-1,1), z/z_norm.view(-1,1), M=M)
