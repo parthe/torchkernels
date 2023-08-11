0418b131c63f9f23a4470d2b2363fd5959cfc67d
Parthe Pandit <parthe@users.noreply.github.com>
2023-08-11 16:56:39 -0700

    Update fmm.py

--- torchkernels/linalg/fmm.py
+++ torchkernels/linalg/fmm.py
@@ -40,15 +40,16 @@ def KmV(K, X, Z, v, out=None, row_chunk_size=None, col_chunk_size=None):
     n_r, n_c = len(X), len(Z)
     b_r = n_r if row_chunk_size is None else row_chunk_size
     b_c = n_c if col_chunk_size is None else col_chunk_size
-
+    return_flag = False
     if out is None:
+        return_flag = True
         out = torch.zeros(n_r, *v.shape[1:])
 
     for i in range(math.ceil(n_r/b_r)):
         for j in range(math.ceil(n_c/b_c)):
              out[i*b_r:(i+1)*b_r] += K(X[i*b_r:(i+1)*b_r], Z[j*b_c:(j+1)*b_c]) @ v[j*b_c:(j+1)*b_c]
 
-    if out is None: return out
+    if return_flag: return out
 
 def KtKmV(K, X, Z, v, out=None, row_chunk_size=None, col_chunk_size=None):
     """
