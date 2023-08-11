f790a9a5ecd688a485939b493cc2f7ef1856deb3
Parthe Pandit <parthe1292@gmail.com>
2023-08-11 16:28:25 -0700

    removed kernel_utils

--- kernel_utils/solvers/mass.py
+++
@@ -1,9 +0,0 @@
-
-def mass(K, X, y):
-    raise NotImplementedError("Work in progress")
-# gm = gm.clone()
-# gm = K(X[bids],X) b - y[bids]
-# d.mul (γ)
-# d[bids] += (η2 − (1 + γ)η1)gm
-# d[bids ] += γ η1 gm
-# b += d
