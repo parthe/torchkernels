6696e8f019bb29a75bc9b345f6147a2497594e48
Parthe Pandit <parthe@users.noreply.github.com>
2023-08-11 08:47:42 -0700

    Update radial.py
    Default 2nd argument of Kernel function is None. Takes 1st argument.

--- kernel_utils/kernels/radial.py
+++ kernel_utils/kernels/radial.py
@@ -1,10 +1,11 @@
 from ..linalg import euclidean
 
-def laplacian(samples, centers, bandwidth=1., M=None):
+def laplacian(samples, centers=None, bandwidth=1., M=None):
     '''
         K(x,z)=exp(-\norm{x-z}_M / bandwidth)
     '''
     assert bandwidth > 0
+    if centers is None: centers = samples
     kernel_mat = euclidean(samples, centers, squared=False, M=M)
     kernel_mat.clamp_(min=0)
     gamma = 1. / bandwidth
@@ -12,11 +13,12 @@ def laplacian(samples, centers, bandwidth=1., M=None):
     kernel_mat.exp_()
     return kernel_mat
 
-def gaussian(samples, centers, bandwidth=1., M=None):
+def gaussian(samples, centers=None, bandwidth=1., M=None):
     '''
         K(x,z)=exp(-\norm{x-z}_M^2 / 2/bandwidth^2)
     '''
     assert bandwidth > 0
+    if centers is None: centers = samples
     kernel_mat = euclidean(samples, centers, squared=True, M=M)
     kernel_mat.clamp_(min=0)
     gamma = 1. / (2 * bandwidth ** 2)
@@ -24,11 +26,12 @@ def gaussian(samples, centers, bandwidth=1., M=None):
     kernel_mat.exp_()
     return kernel_mat
 
-def exp_power(samples, centers, bandwidth=1., power=1., M=None):
+def exp_power(samples, centers=None, bandwidth=1., power=1., M=None):
     '''
         K(x,z)=exp(-\norm{x-z}_M^\gamma / bandwidth)
     '''
     assert bandwidth > 0
+    if centers is None: centers = samples
     kernel_mat = euclidean(samples, centers, squared=True, M=M)
     kernel_mat.pow_(gamma / 2.)
     kernel_mat.mul_(-1. / bandwidth)
