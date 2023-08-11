e684710a255a3598ad11ce985a129fb8676a2f4a
Parthe Pandit <parthe1292@gmail.com>
2023-08-11 16:27:44 -0700

    renamed to pytorch_kernels

---
+++ pytorch_kernels/kernels/__init__.py
@@ -0,0 +1,22 @@
+class Kernel:
+    def __init__(self):
+        self._matrix = None
+        pass
+
+    def __call__(self, value, save=False):
+        if save: 
+            self.matrix = value
+        else:
+            return value
+
+    @property
+    def matrix(self):
+        return self._matrix
+
+    @matrix.setter
+    def matrix(self, value):
+        self._matrix = value
+
+    @matrix.deleter
+    def matrix(self):
+        del self._matrix
---
+++ pytorch_kernels/kernels/generic.py
@@ -0,0 +1,11 @@
+from ..linalg import norm, inner_product
+
+def normalized_inner_product(func, x, z=None, M=None):
+    """
+        K(x,z) = norm(x) * norm(z) * func(<x_, z_>) where x_ = x/norm(x) and z_ = z/norm(z)
+    """
+    if z is None: z = x
+    x_norm = norm(x, M=M)
+    z_norm = norm(z, M=M)
+    xz = inner_product(x/x_norm.view(-1,1), z/z_norm.view(-1,1), M=M)
+    return x_norm.view(-1,1) * func(xz) * z_norm     
