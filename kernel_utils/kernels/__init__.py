8d81f30ff493e359bbffc8520d32ab0175ebc564
Parthe Pandit <parthe@users.noreply.github.com>
2023-08-11 09:58:29 -0700

    Update __init__.py

--- kernel_utils/kernels/__init__.py
+++ kernel_utils/kernels/__init__.py
@@ -1,7 +1,29 @@
 from .radial import laplacian, gaussian, exp_power
+from torch import nn
 
 # Aliases
 exponential_power = exp_power
 dispersal = exp_power
 rbf = gaussian
 laplace = laplacian
+
+
+class Kernel(nn.Module):
+    def __init__(self):
+        self._matrix = None
+        pass
+      
+    def forward(self, x, z=None, save=False):
+        pass
+
+    @property
+    def matrix(self):
+        return self._matrix
+
+    @matrix.setter
+    def matrix(self, value):
+        return self._matrix = value
+
+    @matrix.deleter
+    def matrix(self):
+        del self._matrix
