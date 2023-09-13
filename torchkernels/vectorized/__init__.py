f78cf9e18441cc06afdf1e49db64539e632af421
Parthe Pandit <parthe@users.noreply.github.com>
2023-09-13 09:41:17 -0700

    Update __init__.py

--- torchkernels/vectorized/__init__.py
+++ torchkernels/vectorized/__init__.py
@@ -1,1 +1,9 @@
+from torch.func import vmap, grad
 
+def vectorize(kernel):
+  """
+  Takes a method `kernel` from Rd x Rd -> R
+  """
+  return vmap(
+    vmap(kernel, in_dims=(None, 0)), 
+    in_dims=(0, None))
