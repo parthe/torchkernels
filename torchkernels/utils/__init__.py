09c8c10547eb585b4711d3e28a1ec3a3c2abf9e0
Parthe Pandit <parthe@users.noreply.github.com>
2023-09-13 19:03:55 -0700

    Update __init__.py
    Modified 'vectorize' to be a decorator

--- torchkernels/utils/__init__.py
+++ torchkernels/utils/__init__.py
@@ -1,14 +1,21 @@
 from torch.func import vmap, grad
+import functools
 
-def vectorize(fn):
+
+def vectorize(kernel_fn):
   """
   Vectorizes a bivariate method 
   If `fn` maps inputs (d1,) and (d2,) --> (d3,)
   `vectorize(fn)` maps inputs (n, d1) and (p, d2) --> (n, p, d3)
   """
-  return vmap(
-    vmap(kernel, in_dims=(None, 0)), 
-    in_dims=(0, None))
+  return functools.wraps(
+    vmap(
+        vmap(kernel_fn, in_dims=(None, 0)), 
+        in_dims=(0, None)
+        ),
+    kernel_fn
+    )
     
+@vectorize
 def grad1(kernel):
     return grad(kernel)
