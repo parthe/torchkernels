bfc7fcdfb9177e2dbfd220366d32004fca37403f
Parthe Pandit <parthe@users.noreply.github.com>
2026-05-11 13:36:03 +0530

    Refactor shape matrix and handling its sqrtm

--- torchkernels/feature_maps/rff.py
+++ torchkernels/feature_maps/rff.py
@@ -9,6 +9,7 @@ class RFF:
 		num_features:int, 
 		length_scale:float=1., 
 		shape_matrix:torch.Tensor=None,
+		shape_matrix_sqrtm_fn=torch.linalg.cholesky, # cholesky returns L of shape (d,d) where M = L @ L.T, we want x.T @ M @ z which we apply a as (x @ L) for x of shape (batch_size,d)
 		bias_term:bool=False, 
 		device:str=None,
 		dtype=torch.float32,
@@ -43,14 +44,9 @@ class RFF:
 		self.length_scale = length_scale
 		self.dtype = dtype
 		self.seed = seed
+		self.shape_matrix_sqrtm_fn = shape_matrix_sqrtm_fn
 	
-		if shape_matrix is not None:
-			self.shape_matrix = shape_matrix.to(self.dtype)
-			assert self.shape_matrix.shape[0] == self.shape_matrix.shape[1] == self.input_dim, "Shape matrix must be square and of dimension d x d where d is input dimension."
-			self.sqrt_M = torch.linalg.cholesky(self.shape_matrix.to(self.device))
-		else:
-			self.sqrt_M = None
-			self.shape_matrix = None
+		self.set_shape_matrix_sqrtm(shape_matrix)
 		self.bias_term = bias_term
 
 		self.c1 = torch.sqrt(torch.tensor(2 / self.num_features)).to(self.dtype).to(self.device)
@@ -62,28 +58,43 @@ class RFF:
 			self.torch_gen = torch.Generator(device=self.device).manual_seed(self.seed)
 		else: 
 			self.torch_gen = torch.Generator(device=self.device)
-		self.W1 = (torch.randn(self.input_dim,self._num_features,device=self.device, dtype=self.dtype, generator=self.torch_gen)
-             /self.length_scale).to(self.dtype)
+		self.W1 = torch.randn(self.input_dim,self._num_features,device=self.device, dtype=self.dtype, generator=self.torch_gen)
 		self.set_W2()
 		if self.bias_term:
 			self._bias = (torch.rand(
 				self._num_features, dtype=self.dtype, generator=self.torch_gen
 			) * math.pi * 2).to(self.dtype).to(self.device)
 	
-	def __call__(self, x):
-		assert x.dtype == self.dtype, "Input must be of type {}, else specify dtype parameter".format(self.dtype) 
-		assert x.shape[1] == self.input_dim, "Input must have dimension {}".format(self.input_dim)
-		x = x.to(self.device)
-		if self.shape_matrix is not None:
-			x = torch.mm(x, self.sqrt_M)
-		return self.apply_W2(torch.mm(x, self.W1))
-
+	
 	def set_W1(self, W1=None):
 		if W1 is not None:
 			self.W1=W1
+
+	def set_shape_matrix_sqrtm(self,shape_matrix):
+		if shape_matrix is not None:
+			self.shape_matrix = shape_matrix.to(self.device).to(self.dtype)
+			assert self.shape_matrix.shape[0] == self.shape_matrix.shape[1] == self.input_dim, "Shape matrix must be square and of dimension d x d where d is input dimension."
+			self.shape_matrix_sqrtm = self.shape_matrix_sqrtm_fn(self.shape_matrix)
+		else:
+			self.shape_matrix = None
+			self.shape_matrix_sqrtm = None
+
+	def set_length_scale(self,ell):
+		self.length_scale = ell
 
 	def set_W2(self):
 		raise NotImplementedError("This method must be implemented in the subclass")
 
 	def apply_W2(self):
 		raise NotImplementedError("This method must be implemented in the subclass")
+
+	def apply_W1(self, x):
+		if self.shape_matrix is not None:
+			x = torch.mm(x, self.shape_matrix_sqrtm)
+		return torch.mm(x/self.length_scale, self.W1)
+
+	def __call__(self, x):
+		assert x.dtype == self.dtype, "Input must be of type {}, else specify dtype parameter".format(self.dtype) 
+		assert x.shape[1] == self.input_dim, "Input must have dimension {}".format(self.input_dim)
+		x = x.to(self.device)
+		return self.apply_W2(self.apply_W1(x))
